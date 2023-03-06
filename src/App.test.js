4bee83b959549c88e319d9fc79a97e65864c6d67
Ricardo <ricvillalobosv@gmail.com>
2023-03-06 07:40:40 -0600

    Update App.test.js
    Update test failed.

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,7 @@ import Header from './components/Header';
 
 describe('Prueba en el <Header />', () => {
   test('Debe de mostrar el valor inicial de 5 <Header count={5}/>', () => {
-    const count = 5;
-    render(<Header count={count} />);
+    render(<Header count={10} />);
     expect(screen.getByText(5)).toBeTruthy();
   });
 });
